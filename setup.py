db43c81c89bf4c18bf9bb4b4746e66900094a90e
Олег Правдин <opravidn@gmail.com>
2020-04-11 03:56:26 +0300

    Python version updated

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.3',
+    python_requires='>=3.7',
 )
