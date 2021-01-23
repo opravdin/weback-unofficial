1fb0c36dd789739aa863caa4bec9c16391c48671
Oleg Pravdin <opravdin@gmail.com>
2021-01-23 15:52:16 +0300

    Update package version for pypi

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="weback-unofficial", # Replace with your own username
-    version="0.3.4",
+    version="0.3.5",
     author="Pravdin Oleg",
     author_email="opravdin@gmail.com",
     description="Unofficial client for WeBack API",
