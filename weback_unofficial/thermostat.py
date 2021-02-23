89419232c0669a5db4c6e3a20e47086c0b4e2b19
Matthias Lexer <mail@matthiaslexer.com>
2021-02-23 19:24:26 +0100

    fixing stuff opravdin has requested

--- weback_unofficial/thermostat.py
+++ weback_unofficial/thermostat.py
@@ -6,7 +6,18 @@ AUTO = 'auto'
 MANUAL = 'hand'
 
 WORKING_MODES = {AUTO, MANUAL}
+# temperature is always times two (e.g. 25 degrees = 50 degrees in shadow object)
 TEMP_MULTIPLIER = 2
+
+# temperature is given like 250, which means 25.0 degrees
+TEMP_DISPLAY_DIVIDER = 10
+
+CURRENT_TEMPERATURE = 'air_tem'
+GOAL_TEMPERATURE = 'set_tem'
+CONNECTED = 'connected'
+WORKMODE = 'workmode'
+WORKING_STATUS = 'working_status'
+
 
 class Thermostat(BaseDevice):
     def __init__(self, name, client, shadow=None, description=None, nickname=None):
@@ -15,8 +26,6 @@ class Thermostat(BaseDevice):
     def setMode(self, mode: str):
         if not mode in WORKING_MODES:
             self.raise_invalid_value(WORKING_MODES)
-        
-        print(mode)
         self.publish_single('workmode', mode)
 
     def setTemp(self, temp: str):
@@ -26,23 +35,23 @@ class Thermostat(BaseDevice):
 
     @property
     def is_available(self):
-        return True if self.shadow.get('connected') == 'false' else True
+        return True if self.shadow.get(CONNECTED) == 'false' else True
 
     @property
     def mode(self):
-        return self.shadow.get('workmode')
+        return self.shadow.get(WORKMODE)
 
     @property
     def temperature(self):
-        return self.shadow.get('air_tem') / 10
+        return self.shadow.get(CURRENT_TEMPERATURE) / TEMP_DISPLAY_DIVIDER
 
     @property
     def goal_temperature(self):
-        return self.shadow.get('set_tem') / TEMP_MULTIPLIER
+        return self.shadow.get(GOAL_TEMPERATURE) / TEMP_MULTIPLIER
 
     @property
     def is_heating(self):
-        return True if self.shadow.get('working_status') == 'on' else False    
+        return True if self.shadow.get(WORKING_STATUS) == 'on' else False    
 
     @property
     def autosettings(self):
@@ -56,6 +65,17 @@ class Thermostat(BaseDevice):
             "Sun": self.format_auto_settings('Sun')
         }
 
+    # the shadow object includes a string for every day where the actions are concatenated
+    # this function is used to split them up
+    # input: "04:50_043C,08:00_030C,17:00_043C,20:00_030C,20:00_030C,20:00_030C"
+    # output: [
+    #     "04:50_043C",
+    #     "08:00_030C",
+    #     "17:00_043C",
+    #     "20:00_030C",
+    #     "20:00_030C",
+    #     "20:00_030C"
+    # ]
     def format_auto_settings(self, day):
         command_string = self.shadow.get(day)
         commands = command_string.split(',')
